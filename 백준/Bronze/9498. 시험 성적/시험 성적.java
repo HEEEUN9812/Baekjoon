0058a43ae0a773068d1ec4b317592d8381c08787
HEEEUN9812 <154495684+HEEEUN9812@users.noreply.github.com>
2024-06-15 21:52:45 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        
+        if(n >= 90){
+            bw.write("A" + "\n");
+        }else if(n >= 80){
+            bw.write("B" + "\n");
+        }else if(n >= 70){
+            bw.write("C" + "\n");
+        }else if(n >= 60){
+            bw.write("D" + "\n");
+        }else{
+            bw.write("F" + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
