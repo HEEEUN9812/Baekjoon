310c4001c08202a42f386e3ab39b13ac3b1db13e
HEEEUN9812 <154495684+HEEEUN9812@users.noreply.github.com>
2024-06-29 01:46:35 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 192 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Integer[][] arr1 = new Integer[n][m];
+        Integer[][] arr2 = new Integer[n][m];
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < m; j++){
+                arr1[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < m; j++){
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                bw.write((arr1[i][j] + arr2[i][j]) + " ");
+            }
+            bw.write("\n");
+        }
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
