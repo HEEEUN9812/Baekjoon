1f6ff95b63eada51b3183de6fad1f76acdf399c1
HEEEUN9812 <154495684+HEEEUN9812@users.noreply.github.com>
2025-02-24 01:04:50 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -2,7 +2,7 @@ function solution(age) {
     const ageStr = age +'';
     var answer = '';
     for(let i = 0; i < ageStr.length; i++) {
-        const num = Number(ageStr.charAt(i)) + 97;
+        const num = Number(ageStr[i]) + 97;
         answer += String.fromCharCode(num);
     }
     return answer;
